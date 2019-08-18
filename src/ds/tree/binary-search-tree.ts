7344461aca26b136fc8b9c953b289d0cdb2ccf59
sohammondal <soham.pf.mondal@gmail.com>
2019-08-18 23:30:36 +0530

    ds layout

--- src/ds/tree/binary-search-tree.ts
+++ src/ds/tree/binary-search-tree.ts
@@ -1,7 +1,29 @@
 class _Node {
-
+    left: number;
+    right: number;
+    value: number;
+    constructor(value: number) {
+        this.value = value;
+    }
 }
 
 class BinaryTree {
+    root: null | _Node
+    constructor() {
+        this.root = null;
+    }
 
+    insert(value: number) {
+
+    }
+    lookup(value: number) {
+
+    }
+}
+
+const traverse = (node) => {
+    const tree = { value: node.value, left: null, right: null };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
 }
