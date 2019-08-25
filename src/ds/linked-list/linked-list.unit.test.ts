a1511ac4af8e127a6638bd09a4652ac1daa20717
sohammondal <soham.pf.mondal@gmail.com>
2019-08-26 02:39:08 +0530

    added prepend and insert test cases

--- src/ds/linked-list/linked-list.unit.test.ts
+++ src/ds/linked-list/linked-list.unit.test.ts
@@ -5,7 +5,28 @@ test('is new object Instance of LinkedList', () => {
     expect(myLinkedList).toBeInstanceOf(LinkedList);
 })
 
-test('is append working', () => {
-    const list = myLinkedList.append(1);
-    expect(list.tail.value).toBe(1);
+test('is append() working', () => {
+    let list = myLinkedList.append(5);
+    expect(list.tail.value).toBe(5);
+    list = myLinkedList.append(undefined);
+    expect(list).toBeNull();
+    list = myLinkedList.append(16);
+    expect(list.tail.value).toBe(16);
+})
+
+test('is prepend() working', () => {
+    let list = myLinkedList.prepend(1);
+    expect(list.head.value).toBe(1);
+})
+
+test('is insert() working', () => {
+    try {
+        myLinkedList.insert(2, 99);
+        console.log(myLinkedList.show());
+        expect.arrayContaining(myLinkedList.show()).toEqual([1, 10, 99, 5]);
+        myLinkedList.insert(2, 99);
+    } catch (error) {
+        expect(error.mesaage).toBe(undefined);
+    }
+
 })
