a4796e4342bc1d6c40115359a3dc17af823a15c9
sohammondal <soham.pf.mondal@gmail.com>
2019-08-26 23:19:14 +0530

    implemented testes for show,peek,push,pop

--- src/ds/stacks-and-queues/stacks-using-linked-list.unit.test.ts
+++ src/ds/stacks-and-queues/stacks-using-linked-list.unit.test.ts
@@ -2,7 +2,26 @@ import { Stack } from "./stacks-using-linked-list";
 
 describe("checking operations using Stack", () => {
   const myStack = new Stack();
+
   it("is a instance of Stack", () => {
     expect(myStack).toBeInstanceOf(Stack);
+  });
+
+  it("pushes google to stack", () => {
+    myStack.push("google");
+    expect(myStack.show()).toStrictEqual(["google"]);
+  });
+
+  it("pushes udemy to stack", () => {
+    myStack.push("udemy");
+    expect(myStack.show()).toStrictEqual(["udemy", "google"]);
+  });
+
+  it("peeks at top element in stack", () => {
+    expect(myStack.peek()).toBe("udemy");
+  });
+
+  it("pops top element from stack", () => {
+    expect(myStack.pop()).toBe("udemy");
   });
 });
